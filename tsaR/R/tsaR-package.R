5c4ce9c843f4dbf7d6a833cbe683f04f1c882f0b
Mark Scheuerell <scheuerl@uw.edu>
2022-09-15 16:46:18 -0700

    created Stan package skeleton

---
+++ tsaR/R/tsaR-package.R
@@ -0,0 +1,16 @@
+#' The 'tsaR' package.
+#'
+#' @description A DESCRIPTION OF THE PACKAGE
+#'
+#' @docType package
+#' @name tsaR-package
+#' @aliases tsaR
+#' @useDynLib tsaR, .registration = TRUE
+#' @import methods
+#' @import Rcpp
+#' @importFrom rstan sampling
+#'
+#' @references
+#' Stan Development Team (2021). RStan: the R interface to Stan. R package version 2.21.3. https://mc-stan.org
+#'
+NULL
